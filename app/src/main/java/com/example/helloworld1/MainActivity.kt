d83a4a7ee7eb1984ffaecfff50e4aaf19fcdcc4e
lekhaP <preetilekha4@gmail.com>
2019-08-23 14:34:03 -0400

    Added data binding

--- app/src/main/java/com/example/helloworld1/MainActivity.kt
+++ app/src/main/java/com/example/helloworld1/MainActivity.kt
@@ -1,28 +1,28 @@
 package com.example.helloworld1
 
-import android.support.v7.app.AppCompatActivity
-
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val greetingText : TextView = findViewById(R.id.greetingTextview)
-        val inputView = findViewById<EditText>(R.id.inputTextField)
-        val actionGreet = findViewById<Button>(R.id.greetButton)
-        val actionGoodbye = findViewById<Button>(R.id.GoodByeButton)
 
-        actionGreet.setOnClickListener { btn ->
-            greetingText.text = "Hello, ${inputView.text}!"
+        val binding = DataBindingUtil.setContentView<com.example.helloworld1.databinding.ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.greetButton.setOnClickListener { btn ->
+            binding.greetingTextview.text = "Hello, ${binding.inputTextField.text}!"
         }
 
-        actionGoodbye.setOnClickListener { btn ->
-            greetingText.text = "Goodbye, ${inputView.text}!"
+        binding.GoodByeButton.setOnClickListener { btn ->
+            binding.greetingTextview.text = "Goodbye, ${binding.inputTextField.text}!"
         }
     }
 }
+
+
+
+
+
